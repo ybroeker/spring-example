caaccaa26110ed37406b5a0ed69e0927eb8cc026
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2018-03-06 14:41:27 +0100

    Add Repo and activate JPA

--- src/main/java/gpse/example/ExampleApplication.java
+++ src/main/java/gpse/example/ExampleApplication.java
@@ -2,14 +2,18 @@ package gpse.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 // tag::class[]
-@SpringBootApplication // <1>
+@SpringBootApplication
+@EnableJpaRepositories // <1>
+@EnableTransactionManagement// <2>
 public class ExampleApplication {
+// end::class[]
 
     public static void main(final String... args) {
-        SpringApplication.run(ExampleApplication.class, args); // <2>
+        SpringApplication.run(ExampleApplication.class, args);
     }
 }
-// end::class[]
---
+++ src/main/java/tdpe/example/domain/ArticleRepository.java
@@ -0,0 +1,8 @@
+package tdpe.example.domain;
+
+// tag::class[]
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+}
+// end::class[]
