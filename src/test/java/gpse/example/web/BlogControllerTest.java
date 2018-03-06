5f8930d986dc1943a5b083b4c67ddcd5c110f577
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2018-03-06 11:38:21 +0100

    Fix Unit-Test

--- src/test/java/gpse/example/web/BlogControllerTest.java
+++ src/test/java/gpse/example/web/BlogControllerTest.java
@@ -1,19 +1,33 @@
 package gpse.example.web;
 // tag::test[]
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.web.servlet.ModelAndView;
+import tdpe.example.domain.BlogService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BlogControllerTest {
 
+
+    @Mock //<1>
+    private BlogService blogService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);//<2>
+    }
+
     @Test
     void showBlog() {
-        BlogController blogController = new BlogController();
+        BlogController blogController = new BlogController(blogService);//<3>
 
         ModelAndView modelAndView = blogController.showBlog();
 
-        assertThat(modelAndView.getViewName()).isEqualTo("blog");//<1>
+        assertThat(modelAndView.getViewName()).isEqualTo("blog");
         assertThat(modelAndView.getModel()).containsKey("articles");
     }
 
