68662db2e2343918f410966b2cf014d0b79bf6ad
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2018-03-06 12:01:01 +0100

    Add BlogServiceImpl

---
+++ src/main/java/tdpe/example/domain/BlogServiceImpl.java
@@ -0,0 +1,23 @@
+package tdpe.example.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+// tag::class[]
+@Service //<1>
+public class BlogServiceImpl implements BlogService {
+
+    @Override
+    public List<Article> getArticles() {
+        User user = new User("Uncle Bob", "Bob", "Martin");
+
+        List<Article> articles = new ArrayList<>();
+        articles.add(new Article(user, "A magnificent Article", "Lorem ipsum dolor"));
+        articles.add(new Article(user, "Another Article", "sit amet, consetetur sadipscing elitr"));
+
+        return articles;
+    }
+}
+// end::class[]
