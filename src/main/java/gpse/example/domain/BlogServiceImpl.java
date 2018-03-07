8aa3ad010279f24cde8b4674a6e0d095142b41de
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2018-03-07 01:06:31 +0100

    Fix Checkstyle & PMD-Warnings

--- src/main/java/gpse/example/domain/BlogServiceImpl.java
+++ src/main/java/gpse/example/domain/BlogServiceImpl.java
@@ -1,4 +1,4 @@
-package tdpe.example.domain;
+package gpse.example.domain;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/gpse/example/web/BlogController.java
+++ src/main/java/gpse/example/web/BlogController.java
@@ -1,21 +1,14 @@
 package gpse.example.web;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import gpse.example.domain.Article;
-import gpse.example.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import tdpe.example.domain.Article;
 import gpse.example.domain.BlogService;
 
 // tag::class[]
