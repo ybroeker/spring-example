fd40c3ebf13e0c3b50c81ad93b8d1873587f2e7e
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2019-03-07 01:29:24 +0100

    Remove jaxb-dep

--- src/main/java/gpse/example/domain/BlogService.java
+++ src/main/java/gpse/example/domain/BlogService.java
@@ -2,7 +2,6 @@ package gpse.example.domain;
 
 import java.util.List;
 
-import gpse.example.domain.Article;
 
 // tag::class[]
 public interface BlogService {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO user (username, firstname, lastname) VALUES ('Uncle_Bob', 'Bob', 'Martin');
 
-INSERT INTO article (id, published_at, author_username, title, text) VALUES (1,'2015-12-01 08:00:00.0', 'Uncle_Bob' ,'mein erster eintrag', 'lorem ipsum dolor sit amet...');
-INSERT INTO article (id, published_at, author_username, title, text) VALUES (2, '2015-12-02 08:00:00.0', 'Uncle_Bob', 'mein zweiter eintrag', 'Wants pawn term, dare worsted ladle gull hoe lift wetter murder inner ladle cordage, honor itch offer lodge dock florist. ');
+INSERT INTO article (id, published_at, author_username, title, text) VALUES (1,'2015-12-01 08:00:00.0', 'Uncle_Bob' , 'A magnificent Article', 'Lorem ipsum dolor');
+INSERT INTO article (id, published_at, author_username, title, text) VALUES (2, '2015-12-02 08:00:00.0', 'Uncle_Bob', 'Another Article', 'sit amet, consetetur sadipscing elitr');
